4caadeb6c0f79bdaca03c083acb44ac3c0ae5d6a
Cenzino21 <vincenzolabella2018@gmail.com>
2023-09-17 11:16:46 +0200

    Esercizio Java

---
+++ src/main/java/com/example/ExeSpringBoot/ExeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExeSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExeSpringBoot/ExeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExeSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
